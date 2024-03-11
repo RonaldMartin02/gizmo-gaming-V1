013d8d59fbe2f05a1efe1b21ba178905555011f8
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2024-03-11 19:11:27 -0400

    got it somewhat functional

--- client/src/components/LoggedInHeader.jsx
+++ client/src/components/LoggedInHeader.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
 import Auth from '../utils/auth';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function Header() {
   const handleLogOut = () => {
     Auth.logout()
   }
+  const currentPage = useLocation().pathname;
+  if (currentPage === "/Build/Create") {
+    return (
+      <header>
+        <h2 className='GizmoGaming' onClick={()=>{window.location.href = "/"}}>Gizmo Gaming</h2>
+        <div>
+          <button onClick={()=>{window.location.href = "/"}}>Home</button>
+          <button onClick={handleLogOut}>Sign Out</button>
+        </div>
+      </header>
+    )} else 
+
     return (
       <header>
        <div>
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -4,7 +4,7 @@ import Post from '../components/Post';
 import { GET_ALL_BUILDS } from '../utils/queries.js';
 export default function Landing() {
     const { loading, data } = useQuery(GET_ALL_BUILDS);
-    const posts = data?.builds;
+    const posts = data?.builds || [];
     console.log(posts);
 return (
         <div>
