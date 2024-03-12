d79af26d40f12d3ef0e55bef3ae2e1d8ddab0f78
mighty-little-coder <144641610+mighty-little-coder@users.noreply.github.com>
2024-03-12 01:30:19 -0400

    search is functional

--- client/src/components/LoggedInHeader.jsx
+++ client/src/components/LoggedInHeader.jsx
@@ -3,19 +3,16 @@ import Auth from '../utils/auth';
 import { useLocation } from 'react-router-dom';
 
 export default function Header() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setsearchTerm] = useState('');
   const handleLogOut = () => {
     Auth.logout()
   }
 
   const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-    console.log(searchTerm);
-    console.log(event.target.value);
+    setsearchTerm(event.target.value);
   };
 
   const handleSearch = () => {
-    // Perform the search action here
     window.location.href = `/Search/${searchTerm}`;
   };
 
@@ -23,9 +20,9 @@ export default function Header() {
   if (currentPage === "/Build/Create") {
     return (
       <header>
-        <h2 className='GizmoGaming' onClick={()=>{window.location.href = "/"}}>Gizmo Gaming</h2>
+        <h2 className='GizmoGaming' onClick={() => { window.location.href = "/" }}>Gizmo Gaming</h2>
         <div>
-          <button onClick={()=>{window.location.href = "/"}}>Home</button>
+          <button onClick={() => { window.location.href = "/" }}>Home</button>
           <button onClick={handleLogOut}>Sign Out</button>
         </div>
       </header>
@@ -42,12 +39,12 @@ export default function Header() {
           />
           <button className='search_btn' onClick={handleSearch}>Search</button>
         </div>
-        <h2 className='GizmoGaming' onClick={()=>{window.location.href = "/"}}>Gizmo Gaming</h2>
+        <h2 className='GizmoGaming' onClick={() => { window.location.href = "/" }}>Gizmo Gaming</h2>
         <div>
-          <button onClick={()=>{window.location.href = "./Build/Create"}}>New Post</button>
+          <button onClick={() => { window.location.href = "./Build/Create" }}>New Post</button>
           <button onClick={handleLogOut}>Sign Out</button>
+          <button onClick={() => { window.location.href = "./Aboutus" }}>About Us</button>
         </div>
-        <button onClick={()=>{window.location.href = "./Aboutus"}}>About Us</button>
       </header>
     );
   }
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import Post from '../components/Post';
 import { Link, useLocation } from 'react-router-dom';
@@ -7,24 +6,19 @@ import { GET_ALL_BUILDS } from '../utils/queries.js';
 export default function Search() {
     const location = useLocation();
     const lastSegment = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-
+    const search = lastSegment.replaceAll("%20", " ");
     const { loading, data } = useQuery(GET_ALL_BUILDS, {
         fetchPolicy: "no-cache"
     });
 
-    const [searchTerm, setSearchTerm] = useState(lastSegment);
     const posts = data?.builds || [];
 
     const handleSearch = () => {
         const filteredPosts = posts.filter((post) =>
-            post.game.toLowerCase().includes(searchTerm.toLowerCase()));
+            post.game.toLowerCase().includes(search.toLowerCase()));
 
         return filteredPosts;
     };
-
-    useEffect(() => {
-        setSearchTerm(lastSegment);
-    }, [lastSegment]);
 
     if (loading) {
         return <div>Loading...</div>;
@@ -56,7 +50,7 @@ export default function Search() {
             </div> */}
             <div className='single-project'>
                 {filteredPosts.map((post, index) => (
-                    <Post _id= {post._id} title={post.title} game={post.game} description={post.body} key={index} />
+                    <Post _id={post._id} title={post.title} game={post.game} description={post.body} key={index} />
                 ))}
             </div>
         </div>
