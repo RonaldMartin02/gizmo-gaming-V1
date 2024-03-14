297f483452ac2df0a940cc8b2ed4a313a82861a8
Ronald Martin <ronaldmartin02@gmail.com>
2024-03-14 04:26:41 -0400

    fixed background

--- client/src/components/LoggedInHeader.jsx
+++ client/src/components/LoggedInHeader.jsx
@@ -63,7 +63,7 @@ export default function Header() {
           </div>
           <div className='Header_Btns'>
         {isOpen && (
-          <div className="Header_Hamburger_Items">
+          <div className="Header_Btns">
             <button onClick={() => { window.location.href = '/' }}>Home</button>
             <button onClick={handleLogOut}>Sign Out</button>
             <button onClick={() => { window.location.href = '/Aboutus' }}>About Us</button>
